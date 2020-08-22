84e9d6321c7fcda9e49bcff17f6b93ee530b40e0
Thomas Biesaart <thomas.biesaart@protonmail.com>
2020-08-22 15:37:30 +0200

    Add navbar with electron integration

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import { HomeView } from './views/HomeView';
 import { SettingsView } from './views/SettingsView';
 import { AboutView } from './views/settings/AboutView';
+import {NavBar} from "./views/NavBar";
 
 export const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ export const App: React.FC = () => {
       <Switch>
         <Route exact path="/home" component={HomeView} />
         <Route exact path="/settings" component={SettingsView} />
+        <Route exact path="/navbar" component={NavBar} />
         <Route exact path="/settings/about" component={AboutView} />
         <Redirect to="/home" />
       </Switch>
---
+++ src/views/NavBar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Time} from "../components/Time";
+
+export const NavBar: React.FC = () => (
+    <div className="navbar">
+        <div className="icon-bar">
+            <button className="link" onClick={() => {
+                if (!window.require) {
+                    return;
+                }
+                const electron = window.require('electron');
+                if (!electron) {
+                    return;
+                }
+                electron.ipcRenderer.send('navbar', 'home');
+            }}>
+                <i className="fas fa-home"/>
+            </button>
+        </div>
+        <Time/>
+    </div>
+);
